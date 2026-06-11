c92aef109d2911d3ab81e04071538a2c4a30c6bd
ThihaTineZaw <thihatinezaw.mm@gmail.com>
2026-06-11 23:07:58 +0630

    neon fixed

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
+
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Optimizing Laravel..."
 php artisan optimize:clear
+
 echo "Clearing config..."
 php artisan config:clear
+
 echo "Clearing cache..."
 php artisan cache:clear
+
 echo "Caching config..."
 php artisan config:cache
+
 echo "Caching routes..."
 php artisan route:cache
-echo "Setting up session table..."
-php artisan session:table
-echo "Running migrations..."
-php artisan migrate:fresh --seed
 
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Starting server..."
+php artisan serve --host=0.0.0.0 --port=$PORT
